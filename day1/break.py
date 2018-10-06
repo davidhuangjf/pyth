6d4369a3f2efd3bb43bc21e2ba21a6e254d0001c
david <huangjtold@gmail.com>
2018-10-06 20:39:57 +0800

    break

---
+++ day1/break.py
@@ -0,0 +1,9 @@
+# __author : david
+# date:  2018/10/6
+
+while True:
+    s=input("Enter something :")
+    if s == "quit":
+        break
+    print('Length ot the string is ',len(s))
+print('exit')
