72be0719324e28040e5e0cf512c5886e9c8d678c
david <huangjtold@gmail.com>
2018-10-07 21:45:38 +0800

    param

---
+++ day2/function_param.py
@@ -0,0 +1,16 @@
+# __author : david
+# date:  2018/10/7
+
+def print_max(a,b):
+    if a>b:
+        print(a, 'is maximum')
+    elif a==b:
+        print(a,'is equal to',b)
+    else:
+        print(b,'is maximum')
+
+print_max(400,380)
+
+x=10*100
+y=23*49
+print_max(x,y)
