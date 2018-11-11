49105f9e9d9e76d97634887c756856ca516a456b
david <huangjtold@gmail.com>
2018-11-11 10:48:18 +0800

    default

--- day3/save_file.py
+++ day3/save_file.py
@@ -16,50 +16,26 @@ def save_file(boy,girl,count):
     boy_file.close()
     girl_file.close()
 
+def split_file(file_name):
+    f = open(file_name)
+    boy = []
+    girl = []
+    count = 1
 
-f = open('log.txt')
+    for each_line in f:
+        if each_line[:6] != '======':
+            ( role, line_spoken) = each_line.split(':',1)
+            if role == '小黄':
+                boy.append(line_spoken)
+            if role == '小王':
+                girl.append(line_spoken)
+        else:
+            save_file(boy,girl,count)
+            boy = []
+            girl = []
+            count += 1
 
-boy = []
-girl = []
-count = 1
+        save_file(boy, girl, count)
+    f.close()
 
-for each_line in f:
-    if each_line[:6] != '======':
-        # each_line.s
-        ( role, line_spoken) = each_line.split(':',1)
-        if role == '小黄':
-            boy.append(line_spoken)
-        if role == '小王':
-            girl.append(line_spoken)
-    else:
-        save_file(boy,girl,count)
-        # file_name_boy = 'boy' + str(count) + '.txt'
-        # file_name_girl =  'girl' + str(count) + '.txt'
-        #
-        # boy_file = open(file_name_boy,'w')
-        # girl_file = open(file_name_girl,'w')
-        #
-        # girl_file.writelines(girl)
-        # boy_file.writelines(boy)
-        #
-        # boy_file.close()
-        # girl_file.close()
-
-        boy = []
-        girl = []
-        count += 1
-
-    save_file(boy, girl, count)
-    # file_name_boy = 'boy' + str(count) + '.txt'
-    # file_name_girl = 'girl' + str(count) + '.txt'
-    #
-    # boy_file = open(file_name_boy, 'w')
-    # girl_file = open(file_name_girl, 'w')
-    #
-    # girl_file.writelines(girl)
-    # boy_file.writelines(boy)
-    #
-    # boy_file.close()
-    # girl_file.close()
-
-f.close()
+split_file('log.txt')
