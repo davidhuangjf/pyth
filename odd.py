a7395b031ccffe66fb51da869cdbf5f8b10db373
davidhuangjf <huangjfold@gmail.com>
2018-11-28 16:05:22 +0800

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	day3/log.txt
    #	day3/save_file.py

--- odd.py
+++ odd.py
@@ -1,21 +1,20 @@
-# for i in range(101):
-#     if i%2 != 0:
-#         print(i, end=" ")
-#         i += 1
-#     else:
-#         i += 1
-
-
-x = 7
-i = 1
-flag = 0
-while i <= 100:
-    if (x % 2 == 1) and (x % 3 == 2) and (x % 5 == 4) and (x % 6 == 5):
-        flag = 1
+for i in range(1, 101):
+    if i%2 != 0:
+        print(i, end=" ")
+        i += 1
     else:
-        x = 7 * (i+1) # 根据题意，x一定是7的整数倍，所以每次乘以7
-    i += 1
-if flag == 1:
-    print('阶梯数是：', x)
-else:
-    print('在程序限定的范围内找不到答案！')
+        i += 1
+
+# x = 7
+# i = 1
+# flag = 0
+# while i <= 100:
+#     if (x % 2 == 1) and (x % 3 == 2) and (x % 5 == 4) and (x % 6 == 5):
+#         flag = 1
+#     else:
+#         x = 7 * (i+1) # 根据题意，x一定是7的整数倍，所以每次乘以7
+#     i += 1
+# if flag == 1:
+#     print('阶梯数是：', x)
+# else:
+#     print('在程序限定的范围内找不到答案！')
