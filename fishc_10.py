0aea6db6475b9d202beb8b410026cd248e32b0bf
root <huangjfold@gmail.com>
2018-11-29 21:02:30 +0800

    fishc_10

--- fishc_10.py
+++ fishc_10.py
@@ -22,3 +22,9 @@ for each in range(len(member)):
     if each % 2 == 0:
         print(member[each], member[each+1])
 
+
+count = 0
+length = len(member)
+while count < length:
+    print(member[count], member[count+1])
+    count += 2
